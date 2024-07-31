1984d67a290ece7369b3e25e1b252702f5f28a48
Osman Ali Yardım <osmanaliyardim@gmail.com>
2024-07-31 21:01:03 +0300

    Implement CQRS abstractions for Product listing

---
+++ Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
@@ -0,0 +1,19 @@
+﻿namespace eCommerceMicroservicesV2.Catalog.API.Products.GetProducts;
+
+public record GetProductsQuery() : IQuery<GetProductsResult>;
+
+public record GetProductsResult(IEnumerable<Product> Products);
+
+internal class GetProductsQueryHandler(IDocumentSession sesion, ILogger<GetProductsQueryHandler> logger)
+    : IQueryHandler<GetProductsQuery, GetProductsResult>
+{
+    public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
+    {
+        var productsToList = await sesion.Query<Product>().ToListAsync(cancellationToken);
+
+        if (productsToList == null)
+            logger.LogError("Problem with getting products from CatalogDB");
+ 
+        return new GetProductsResult(productsToList!);
+    }
+}
