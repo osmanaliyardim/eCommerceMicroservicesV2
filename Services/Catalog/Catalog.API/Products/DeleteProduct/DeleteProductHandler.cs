67a159ac69dc2c8e46be18279d8ff48328515287
Osman Ali Yardım <osmanaliyardim@gmail.com>
2024-07-31 22:31:11 +0300

    Implement CQRS abstractions for Product removal

---
+++ Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
@@ -0,0 +1,35 @@
+﻿namespace eCommerceMicroservicesV2.Catalog.API.Products.CreateProduct;
+
+public record DeleteProductCommand(
+    Guid Id
+) : ICommand<DeleteProductResult>;
+
+public record DeleteProductResult();
+
+internal class DeleteProductCommandHandler(IDocumentSession session, ILogger<DeleteProductCommandHandler> logger)
+    : ICommandHandler<DeleteProductCommand, DeleteProductResult>
+{
+    public async Task<DeleteProductResult> Handle(DeleteProductCommand command, CancellationToken cancellationToken)
+    {
+        var productToDelete = await session.LoadAsync<Product>(command.Id, cancellationToken);
+
+        if (productToDelete is null)
+        {
+            logger.LogError("Product not found to delete");
+
+            throw new ProductNotFoundException();
+        }
+
+        try
+        {
+            session.Delete(productToDelete!);
+            await session.SaveChangesAsync(cancellationToken);
+        }
+        catch (Exception)
+        {
+            logger.LogError("Problem with deleting product from CatalogDB");
+        }
+
+        return new DeleteProductResult();
+    }
+}
