307273fe0a14c86225a167ee94807aa9927b7673
Osman Ali Yardım <osmanaliyardim@gmail.com>
2024-07-31 22:31:57 +0300

    Create DELETE endpoint for products

---
+++ Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductEndpoint.cs
@@ -0,0 +1,20 @@
+﻿namespace eCommerceMicroservicesV2.Catalog.API.Products.CreateProduct;
+
+public class DeleteProductEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapDelete("/products/{id}",
+            async (Guid id, ISender sender) =>
+            {
+                var result = await sender.Send(new DeleteProductCommand(id));
+
+                return Results.NoContent();
+            })
+        .WithName("DeleteProduct")
+        .Produces(StatusCodes.Status204NoContent)
+        .ProducesProblem(StatusCodes.Status404NotFound)
+        .WithSummary("Delete Product")
+        .WithDescription("Delete Product");
+    }
+}
