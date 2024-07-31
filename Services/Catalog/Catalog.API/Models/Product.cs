62853cbaae113dff9469b7e9337bb4607a2d428a
Osman Ali Yardım <osmanaliyardim@gmail.com>
2024-07-31 17:04:08 +0300

    Create Product entity

---
+++ Services/Catalog/Catalog.API/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿namespace eCommerceMicroservicesV2.Catalog.API.Models;
+
+public class Product
+{
+    public Guid Id { get; set; }
+
+    public string Name { get; set; } = default!;
+
+    public List<string> Categories { get; set; } = new();
+
+    public string Description { get; set; } = default!;
+
+    public string ImageFile { get; set; } = default!;
+
+    public decimal Price { get; set; }
+}
