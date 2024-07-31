4730e4c80e41fa3053c6df4c268bba462c0e1ecd
Osman Ali Yardım <osmanaliyardim@gmail.com>
2024-07-31 17:29:17 +0300

    Create abstraction on MediatR for CQRS

---
+++ BuildingBlocks/BuildingBlocks/CQRS/IQueryHandler.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace eCommerceMicroservices2.BuildingBlocks.CQRS;
+
+public interface IQueryHandler<in TQuery, TResponse> : IRequestHandler<TQuery, TResponse>
+    where TQuery : IQuery<TResponse>
+    where TResponse : notnull
+{
+
+}
