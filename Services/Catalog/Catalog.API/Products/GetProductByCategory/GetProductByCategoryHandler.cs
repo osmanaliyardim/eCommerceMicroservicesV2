cffbbbf0cdeaf8fccfdf69fbe78b539a0f698675
Osman Ali Yardım <osmanaliyardim@gmail.com>
2024-08-01 09:33:26 +0300

    Implement CQRS abstractions for ProductByCategory

---
+++ Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryHandler.cs
@@ -0,0 +1,30 @@
+﻿namespace eCommerceMicroservicesV2.Catalog.API.Products.GetProductByCategory;
+
+public record GetProductByCategoryQuery(string Categories) : IQuery<GetProductByCategoryResult>;
+
+public record GetProductByCategoryResult(IEnumerable<Product> Products);
+
+internal class GetProductByCategoryQueryHandler(IDocumentSession sesion, ILogger<GetProductByCategoryQueryHandler> logger)
+    : IQueryHandler<GetProductByCategoryQuery, GetProductByCategoryResult>
+{
+    public async Task<GetProductByCategoryResult> Handle(GetProductByCategoryQuery query, CancellationToken cancellationToken)
+    {
+        var categories = query.Categories
+            .Split(',')    // "Category A, Category B" => ["Category A", "Category B"]
+            .Select(c => c.TrimStart().TrimEnd())    // ["  Category A  ", "   Category B   "] => ["Category A", "Category B"]
+            .ToArray();
+
+        var productsToList = await sesion.Query<Product>()
+            .Where(product => product.Categories.Any(category => categories.Contains(category)))
+            .ToListAsync(cancellationToken);
+
+        if (productsToList is null)
+        {
+            logger.LogError("Problem with getting products by category from CatalogDB");
+
+            throw new ProductNotFoundException();
+        }
+
+        return new GetProductByCategoryResult(productsToList);
+    }
+}
