aec2c085618eaa7564b53717284676472a311d78
Osman Ali Yardım <osmanaliyardim@gmail.com>
2024-08-01 09:58:06 +0300

    Implement CQRS abstractions for Product update

---
+++ Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
@@ -0,0 +1,41 @@
+﻿namespace eCommerceMicroservicesV2.Catalog.API.Products.UpdateProduct;
+
+public record UpdateProductCommand(Guid Id, string Name, List<string> Categories, string Description,
+    string ImageFile, decimal Price
+) : ICommand<UpdateProductResult>;
+
+public record UpdateProductResult(Product Product);
+
+public class UpdateProductCommandHandler(IDocumentSession session, ILogger<UpdateProductCommandHandler> logger)
+    : ICommandHandler<UpdateProductCommand, UpdateProductResult>
+{
+    public async Task<UpdateProductResult> Handle(UpdateProductCommand command, CancellationToken cancellationToken)
+    {
+        var productToUpdate = await session.LoadAsync<Product>(command.Id, cancellationToken);
+
+        if (productToUpdate == null)
+        {
+            logger.LogError("Problem with getting product for update from CatalogDB");
+
+            throw new ProductNotFoundException();
+        }
+
+        productToUpdate.Name = command.Name;
+        productToUpdate.Description = command.Description;
+        productToUpdate.ImageFile = command.ImageFile;
+        productToUpdate.Price = command.Price;
+        productToUpdate.Categories = command.Categories;
+
+        try
+        {
+            session.Update(productToUpdate);
+            await session.SaveChangesAsync(cancellationToken);
+        }
+        catch (Exception)
+        {
+            logger.LogError("Problem with updating product from CatalogDB");
+        }
+
+        return new UpdateProductResult(productToUpdate);
+    }
+}
