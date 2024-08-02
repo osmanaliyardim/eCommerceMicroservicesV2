8b8ced702b7c95e0e5037371b890a213eafb1e5d
Osman Ali Yardım <osmanaliyardim@gmail.com>
2024-08-02 08:23:11 +0300

    Create entities for Basket

---
+++ Services/Basket/Basket.API/Models/ShoppingCart.cs
@@ -0,0 +1,21 @@
+﻿namespace eCommerceMicroservicesV2.Basket.API.Models;
+
+public class ShoppingCart
+{
+    public string UserName { get; set; } = default!;
+
+    public List<ShoppingCartItem> Items { get; set; } = new();
+
+    public decimal TotalPrice => Items.Sum(item => item.Price * item.Quantity);
+
+    public ShoppingCart(string userName)
+    {
+        UserName = userName;
+    }
+
+    // Required for Mapping
+    public ShoppingCart()
+    {
+        
+    }
+}
