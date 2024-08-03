b63b47dc0f396f3f2ce285786aaab5ff28888946
Osman Ali Yardım <osmanaliyardim@gmail.com>
2024-08-03 21:21:19 +0300

    Implement CQRS abstractions for Order creation

--- src/Services/Ordering/Ordering.Application/GlobalUsing.cs
+++ src/Services/Ordering/Ordering.Application/GlobalUsing.cs
@@ -1,1 +1,6 @@
-﻿
+﻿global using eCommerceMicroservicesV2.BuildingBlocks.CQRS;
+global using eCommerceMicroservicesV2.Ordering.Application.Dtos;
+global using eCommerceMicroservicesV2.Ordering.Domain.Models;
+global using FluentValidation;
+global using eCommerceMicroservicesV2.Ordering.Application.Data;
+global using eCommerceMicroservicesV2.Ordering.Domain.ValueObjects;
---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/CreateOrderCommand.cs
@@ -0,0 +1,26 @@
+﻿namespace eCommerceMicroservicesV2.Ordering.Application.Orders.Commands;
+
+public record CreateOrderResult(OrderDto OrderDto);
+
+public class CreateOrderCommandValidator : AbstractValidator<CreateOrderCommand>
+{
+    public CreateOrderCommandValidator()
+    {
+        RuleFor(x => x.OrderDto.OrderName)
+            .NotEmpty()
+                .WithMessage("Name is required.");
+
+        RuleFor(x => x.OrderDto.CustomerId)
+            .NotNull()
+                .WithMessage("CustomerId is required.");
+
+        RuleFor(x => x.OrderDto.OrderItems)
+            .NotEmpty()
+                .WithMessage("OrderItems should not be empty.");
+    }
+}
+
+public record CreateOrderCommand(OrderDto OrderDto) : ICommand<CreateOrderResult>
+{
+
+}
