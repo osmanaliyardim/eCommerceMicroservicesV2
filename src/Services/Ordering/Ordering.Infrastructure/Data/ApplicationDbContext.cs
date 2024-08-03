ccc8d07543899968599aa9d076674405efeb6fe1
Osman Ali Yardım <osmanaliyardim@gmail.com>
2024-08-03 13:43:08 +0300

    Create DBContext for Orders

---
+++ src/Services/Ordering/Ordering.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+
+namespace eCommerceMicroservicesV2.Ordering.Infrastructure.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<Customer> Customers => Set<Customer>();
+
+    public DbSet<Product> Products => Set<Product>();
+
+    public DbSet<Order> Orders => Set<Order>();
+
+    public DbSet<OrderItem> OrderItems => Set<OrderItem>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
