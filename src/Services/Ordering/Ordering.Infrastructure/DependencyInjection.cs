386629f860d25f1389908add6d5783e0a9eeb257
Osman Ali Yardım <osmanaliyardim@gmail.com>
2024-08-03 14:11:03 +0300

    Register SQL Server DbContext for Orders

--- src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using eCommerceMicroservicesV2.Ordering.Infrastructure.Data;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace eCommerceMicroservicesV2.Ordering.Infrastructure;
@@ -8,6 +9,11 @@ public static class DependencyInjection
     public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
     {
         var connStr = configuration.GetConnectionString(Messages.ORDERING_DB_NAME);
+
+        services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            options.UseSqlServer(connStr);
+        });
 
         return services;
     }
