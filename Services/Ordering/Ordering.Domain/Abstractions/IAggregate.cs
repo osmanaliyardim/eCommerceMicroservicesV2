8dcd2b972dbe66fac3235720781af0939e16adc6
Osman Ali Yardım <osmanaliyardim@gmail.com>
2024-08-03 11:29:34 +0300

    Add DDD abstractions
    Adding DDD abstractions (Entity, IEntity, Aggregate, IAggregate, IDomainEvent) for Ordering.Domain layer

---
+++ Services/Ordering/Ordering.Domain/Abstractions/IAggregate.cs
@@ -0,0 +1,13 @@
+﻿namespace eCommerceMicroservicesV2.Ordering.Domain.Abstractions;
+
+public interface IAggregate<T> : IAggregate, IEntity<T>
+{
+
+}
+
+public interface IAggregate : IEntity
+{
+    IReadOnlyList<IDomainEvent> DomainEvents { get; }
+
+    IDomainEvent[] ClearDomainEvents();
+}
