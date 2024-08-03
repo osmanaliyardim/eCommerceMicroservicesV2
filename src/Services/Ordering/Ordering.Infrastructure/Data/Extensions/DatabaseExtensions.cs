285a3bfe80d728b785387868f7f9f964aae4fdd0
Osman Ali Yardım <osmanaliyardim@gmail.com>
2024-08-03 14:55:44 +0300

    Automize seeding database for OrderingDB

--- src/Services/Ordering/Ordering.Infrastructure/Data/Extensions/DatabaseExtensions.cs
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Extensions/DatabaseExtensions.cs
@@ -12,5 +12,41 @@ public static class DatabaseExtensions
         var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
 
         await context.Database.MigrateAsync();
+
+        await SeedAsync(context);
+    }
+
+    private static async Task SeedAsync(ApplicationDbContext context)
+    {
+        await SeedCustomerAsync(context);
+        await SeedProductAsync(context);
+        await SeedOrderAndItemsAsync(context);
+    }
+
+    private static async Task SeedCustomerAsync(ApplicationDbContext context)
+    {
+        if (!await context.Customers.AnyAsync())
+        {
+            await context.Customers.AddRangeAsync(InitialData.Customers);
+            await context.SaveChangesAsync();
+        }
+    }
+
+    private static async Task SeedProductAsync(ApplicationDbContext context)
+    {
+        if (!await context.Products.AnyAsync())
+        {
+            await context.Products.AddRangeAsync(InitialData.Products);
+            await context.SaveChangesAsync();
+        }
+    }
+
+    private static async Task SeedOrderAndItemsAsync(ApplicationDbContext context)
+    {
+        if (!await context.Orders.AnyAsync())
+        {
+            await context.Orders.AddRangeAsync(InitialData.OrdersWithItems);
+            await context.SaveChangesAsync();
+        }
     }
 }
