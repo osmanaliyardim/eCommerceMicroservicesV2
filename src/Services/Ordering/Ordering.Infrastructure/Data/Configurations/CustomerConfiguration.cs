8b724d8a759ed89598b1b26fe5ab2c27e5bd5221
Osman Ali Yardım <osmanaliyardim@gmail.com>
2024-08-03 14:05:31 +0300

    Adding entity configurations for Order Models

---
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Configurations/CustomerConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using eCommerceMicroservicesV2.Ordering.Domain.ValueObjects;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace eCommerceMicroservicesV2.Ordering.Infrastructure.Data.Configurations;
+
+public class CustomerConfiguration : IEntityTypeConfiguration<Customer>
+{
+    public void Configure(EntityTypeBuilder<Customer> builder)
+    {
+        builder.HasKey(c => c.Id);
+        builder.Property(c => c.Id)
+            .HasConversion
+            (
+                customerId => customerId.Value,
+                dbId => CustomerId.Of(dbId)
+            );
+
+        builder.Property(c => c.Name).HasMaxLength(100).IsRequired();
+
+        builder.Property(c => c.Email).HasMaxLength(255);
+        builder.HasIndex(c => c.Email).IsUnique();
+    }
+}
