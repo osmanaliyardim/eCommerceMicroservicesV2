ed57335cd0be70efd9e7c288c6c9170e0fcd2807
Osman Ali Yardım <osmanaliyardim@gmail.com>
2024-08-04 14:03:18 +0300

    Register MassTransit.RabbitMQ to Ordering Service

--- src/Services/Ordering/Ordering.Application/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Application/DependencyInjection.cs
@@ -1,4 +1,6 @@
 ﻿using eCommerceMicroservicesV2.BuildingBlocks.Behaviors;
+using eCommerceMicroservicesV2.BuildingBlocks.Messaging.MassTransit;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
 
@@ -6,7 +8,8 @@ namespace eCommerceMicroservicesV2.Ordering.Application;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    public static IServiceCollection AddApplicationServices
+        (this IServiceCollection services, IConfiguration configuration)
     {
         services.AddMediatR(config =>
         {
@@ -14,6 +17,8 @@ public static class DependencyInjection
             config.AddOpenBehavior(typeof(ValidationBehavior<,>));
             config.AddOpenBehavior(typeof(LoggingBehavior<,>));
         });
+
+        services.AddMessageBroker(configuration, Assembly.GetExecutingAssembly());
 
         return services;
     }
