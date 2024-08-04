a370fea4d1e2a85080eb6c5f27d56a8f4f1840d8
Osman Ali Yardım <osmanaliyardim@gmail.com>
2024-08-04 09:03:42 +0300

    Add Carter to Ordering.API

--- src/Services/Ordering/Ordering.API/GlobalUsing.cs
+++ src/Services/Ordering/Ordering.API/GlobalUsing.cs
@@ -2,3 +2,4 @@
 global using eCommerceMicroservicesV2.Ordering.Application;
 global using eCommerceMicroservicesV2.Ordering.Infrastructure;
 global using eCommerceMicroservicesV2.Ordering.Infrastructure.Data.Extensions;
+global using Carter;
