b773581f59054e703ea97dd2b70944af2a96f140
Osman Ali Yardım <osmanaliyardim@gmail.com>
2024-08-04 08:19:03 +0300

    Implement CQRS abstractions for OrdersByName

---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByName/GetOrdersByNameHandler.cs
@@ -0,0 +1,21 @@
+﻿using eCommerceMicroservicesV2.Ordering.Application.Extensions;
+
+namespace eCommerceMicroservicesV2.Ordering.Application.Orders.Queries.GetOrderByName;
+
+public class GetOrdersByNameHandler(IApplicationDbContext context)
+    : IQueryHandler<GetOrdersByNameQuery, GetOrdersByNameResult>
+{
+    public async Task<GetOrdersByNameResult> Handle(GetOrdersByNameQuery query, CancellationToken cancellationToken)
+    {
+        var orderName = OrderName.Of(query.Name);
+        
+        var orders = await context.Orders
+            .Include(o => o.OrderItems)
+                .Where(o => o.OrderName.Value.Contains(query.Name))
+                    .OrderBy(o => o.OrderName.Value)
+                        .AsNoTracking()
+                            .ToListAsync(cancellationToken);
+
+        return new GetOrdersByNameResult(orders.ToOrderDtoList());
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByName/GetOrdersByNameQuery.cs
@@ -0,0 +1,8 @@
+﻿namespace eCommerceMicroservicesV2.Ordering.Application.Orders.Queries.GetOrderByName;
+
+public record GetOrdersByNameResult(IEnumerable<OrderDto> OrderDtos);
+
+public record GetOrdersByNameQuery(string Name) : IQuery<GetOrdersByNameResult>
+{
+
+}
