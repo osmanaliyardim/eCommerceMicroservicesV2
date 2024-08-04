9d57a505036aa4dc5975ebfb6ca94de6d04888bd
Osman Ali Yardım <osmanaliyardim@gmail.com>
2024-08-04 08:19:16 +0300

    Implement CQRS abstractions for OrdersByCustomer

---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByCustomer/GetOrdersByCustomerQuery.cs
@@ -0,0 +1,8 @@
+﻿namespace eCommerceMicroservicesV2.Ordering.Application.Orders.Queries.GetOrdersByCustomer;
+
+public record GetOrdersByCustomerResult(IEnumerable<OrderDto> CustomerDtos);
+
+public record GetOrdersByCustomerQuery(Guid CustomerId) : IQuery<GetOrdersByCustomerResult>
+{
+
+}
